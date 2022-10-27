a332483de1568466f576b4fa7f8b88c869e92faa
SHOUKAT <shoukatrahman2001@gmail.com>
2022-10-27 00:48:02 -0700

    Cal

---
+++ Console11/Console11/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Console11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt_first = new System.Windows.Forms.TextBox();
+            this.txt_second = new System.Windows.Forms.TextBox();
+            this.btn_sum = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_answer = new System.Windows.Forms.TextBox();
+            this.btn_sub = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_first
+            // 
+            this.txt_first.Location = new System.Drawing.Point(280, 82);
+            this.txt_first.Name = "txt_first";
+            this.txt_first.Size = new System.Drawing.Size(182, 20);
+            this.txt_first.TabIndex = 0;
+            // 
+            // txt_second
+            // 
+            this.txt_second.Location = new System.Drawing.Point(280, 152);
+            this.txt_second.Name = "txt_second";
+            this.txt_second.Size = new System.Drawing.Size(182, 20);
+            this.txt_second.TabIndex = 1;
+            // 
+            // btn_sum
+            // 
+            this.btn_sum.Location = new System.Drawing.Point(312, 198);
+            this.btn_sum.Name = "btn_sum";
+            this.btn_sum.Size = new System.Drawing.Size(75, 23);
+            this.btn_sum.TabIndex = 4;
+            this.btn_sum.Text = "Sum";
+            this.btn_sum.UseVisualStyleBackColor = true;
+            this.btn_sum.Click += new System.EventHandler(this.btn_sum_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "First value";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Second value";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(125, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Answer";
+            // 
+            // txt_answer
+            // 
+            this.txt_answer.Location = new System.Drawing.Point(280, 242);
+            this.txt_answer.Name = "txt_answer";
+            this.txt_answer.Size = new System.Drawing.Size(182, 20);
+            this.txt_answer.TabIndex = 8;
+            // 
+            // btn_sub
+            // 
+            this.btn_sub.Location = new System.Drawing.Point(434, 198);
+            this.btn_sub.Name = "btn_sub";
+            this.btn_sub.Size = new System.Drawing.Size(75, 23);
+            this.btn_sub.TabIndex = 9;
+            this.btn_sub.Text = "sub";
+            this.btn_sub.UseVisualStyleBackColor = true;
+            this.btn_sub.Click += new System.EventHandler(this.btn_sub_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(471, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 427);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_sub);
+            this.Controls.Add(this.txt_answer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_sum);
+            this.Controls.Add(this.txt_second);
+            this.Controls.Add(this.txt_first);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_first;
+        private System.Windows.Forms.TextBox txt_second;
+        private System.Windows.Forms.Button btn_sum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_answer;
+        private System.Windows.Forms.Button btn_sub;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Console11/Console11/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Console11
+{
+    public partial class Form1 : Form
+    {
+        Simple myCal = new Simple();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_sum_Click(object sender, EventArgs e)
+        {
+            txt_answer.Text = myCal.Add(Convert.ToInt32(txt_first.Text), Convert.ToInt32(txt_second.Text)).ToString();
+            txt_first.Clear();
+        }
+
+        private void btn_sub_Click(object sender, EventArgs e)
+        {
+            txt_answer.Text = myCal.Sub(Convert.ToInt32(txt_first.Text), Convert.ToInt32(txt_second.Text)).ToString();
+
+        }
+        private void Clear()
+        {
+            txt_first.Clear();
+            txt_second.Clear();
+            txt_answer.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+       
+    }
+}
